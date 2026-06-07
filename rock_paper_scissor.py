cea7391cf749759edc7d17804a2fa610e6c0f97b
Saundarya <singhshona1311@gmail.com>
2026-06-07 18:25:42 +0530

    world's smallest esports game

---
+++ rock_paper_scissor.py
@@ -0,0 +1,38 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissor"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissor or Q to quit:").lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # 0 for rock, 1 for paper, 2 for scissor
+
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+    if user_input == "rock" and computer_pick == "scissor":
+        print("You win!")
+        user_wins+=1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You win!")
+        user_wins+=1
+    elif user_input == "scissor" and computer_pick == "paper":
+        print("You win!")
+        user_wins+=1
+    elif user_input == computer_pick:
+        print("It's a tie!")
+    else:
+        print("You lose!")
+        computer_wins+=1
+    print("You won", user_wins, "times.")
+    print("Computer won", computer_wins, "times.")
+print("Final score: You", user_wins, "Computer", computer_wins)
+
+
